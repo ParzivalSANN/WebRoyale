799d678ad62678492c3ba3c7f02179b887b1dd1b
Your Name <you@example.com>
2025-12-17 16:42:52 +0300

    fix: vercel static serving paths

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
 // Serve Static Files
-app.use(express.static(__dirname));
+app.use(express.static(path.join(process.cwd())));
 
 // Fallback to index.html for SPA-like behavior
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(process.cwd(), 'index.html'));
 });
 
 server.listen(PORT, () => {
